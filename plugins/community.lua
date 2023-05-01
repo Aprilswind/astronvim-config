9e1103e8d8e6cabcd593482b783c293f3e3b3a61
aprilswind <joel.sr1024@gmail.com>
2023-05-01 14:56:20 +0530

    added colorschemes

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,66 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.nightfox", enabled = true },
+  { import = "astrocommunity.colorscheme.kanagawa", enabled = true },
+  { import = "astrocommunity.colorscheme.rose-pine", enabled = true },
+  { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
+  { import = "astrocommunity.colorscheme.drakula", enabled = false },
+  { import = "astrocommunity.colorscheme.everforest", enabled = true },
+  { import = "astrocommunity.colorscheme.gruvbox", enabled = true },
+  { import = "astrocommunity.colorscheme.minibase-16", enabled = false },
+  { import = "astrocommunity.colorscheme.monokai-pro", enabled = true },
+  { import = "astrocommunity.colorscheme.onigiri", enabled = true },
+  { import = "astrocommunity.colorscheme.oxocarbon", enabled = true },
+  { import = "astrocommunity.colorscheme.poimandres", enabled = true },
+  { import = "astrocommunity.colorscheme.tokyonight", enabled = true },
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        ts_rainbow2 = true,
+      },
+    },
+  },
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+        require("monokai-pro").setup {
+        filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
+    }
+    end
+},
+  { import = "astrocommunity.completion.copilot-lua" },
+  { -- further customize the options set by the community
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
 }
